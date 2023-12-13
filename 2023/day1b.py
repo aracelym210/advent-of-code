1c65d83d04f65bc20ce43df372d9bef9fcea862d
aracelym <2760319+aracelym210@users.noreply.github.com>
2023-12-13 00:05:08 -0600

    works on sample, but not larger calibration value dataset

--- 2023/day1b.py
+++ 2023/day1b.py
@@ -22,14 +22,17 @@
     refs: used ChatGPT to help identify logic flaws I was running into with first attempt at a solution
 
 """
+import re
 
 def main():
-    lines = read_lines('inputs/day1b-sample.txt')
-    print(lines)
+    lines = read_lines('inputs/day1a.txt')
+    mod_lines = []
 
-    cleaned_lines = str_to_digit_handler(lines)
-
-    print(cleaned_lines)
+    for line in lines:
+        cleaned_line = clean_line(line)
+        mod_lines.append(cleaned_line)
+        
+    add(extract_digits(mod_lines))
 
 def read_lines(filename):
     """
@@ -44,96 +47,45 @@ def read_lines(filename):
     
     return lines
 
-def str_to_digit_handler(lines):
-    """
-        Using a series of if statements, iterate through each line apply 
-        fixes when necessary 
+def clean_line(line):
+    number_map = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
 
-        Returns a new list with cleaned values to later be used for extracting and 
-        summing values
-    """
-    values_to_extract = []
-    for line in lines:
-        if line[0].isdigit() and line[-1].isdigit():
-            values_to_extract.append(line)
-            #print("first and last characters are digits.")
-        else:
-            cleaned_line = str_to_digit(line)
-            values_to_extract.append(cleaned_line)
-            #print(f"niether the first or last characters are digits.")
+    # Create a regex pattern to match number words. Equivalent to one|two|three... etc
+    pattern = re.compile('|'.join(number_map.keys()))
+    print(type(pattern))
 
-    return values_to_extract
+    # Replace each matched number word with its corresponding digit using a nested function
+    def replace(match):
+        word = match.group(0)
+        return str(number_map[word])
+
+    # the regex pattern object's method "sub" can take either a string or function as it's 
+    # first argument. Here, we reference the replace method defined above 
+    cleaned_line = pattern.sub(replace, line)
+
+    print(f"Original line: {line}")
+    print(f"Cleaned line: {cleaned_line}\n\n")
+
+    return cleaned_line
 
 
-def str_to_digit(line):
-    """
-        sort number map so that we check for longer length number strings before shorter ones
-        this will handle edge cases like eightwothree != eight2three since "two" is before "eight" in number_map
-        str_num[0] refers to the first element in the tuple. (i.e. (one, 1) -> str[0] == one)
-    """
-    number_map = { 'one':1, 'two': 2, 'three':3 , 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9 }
-    sorted_numbers = sorted(number_map.items(), key= lambda str_num: len(str_num[0]), reverse=True)
+def extract_digits(mod_lines):
+    clean_digits = []
 
-    clean_line = line
-    for string_num, number in sorted_numbers:
-        clean_line = clean_line.replace(string_num, str(number))
+    for digit in mod_lines:
+        clean_digits.append(re.findall(r'\d', digit))
     
-    return clean_line
+    return clean_digits
 
-
-def generate_cv_array(lines):
-    """
-        This function exists only to append values to array
-        Value extraction handled in separate function extract_calibration_values()
-    """
-    calibration_values = []
-    
-    # if line does not have numbers, skip extraction (i.e. eighttwothree)
-    for line in lines:
-        if line.isalpha():
-            continue
-        else:
-            c_value = extract_calibration_values(line)
-            calibration_values.append(c_value)
-
-    return calibration_values
-
-
-def extract_calibration_values(line):
-    """
-        Looks for first digit in line(str) and appends to temp variable
-        Then looks for the last digit by iterating through the line in reverse and appends that
-        to the same temp variable
-    """
-    c_value = ''
-    reverse = line[::-1]
-
-    for char in line:
-        if char.isdigit():
-            c_value += char
-            break 
-
-    for char in reverse:
-        if char.isdigit():
-            c_value += char
-            break     
-    
-    c_value = int(c_value)
-    
-    return c_value
-
-
-def add_calibration_values(calibration_values):
+def add(clean_digits):
     sum = 0
+    for num in clean_digits:
+        if len(num) > 1:
+            sum += int(num[0] + num[-1])
+        if len(num) == 1:
+            sum += int(num[0][0] + num[0][-1])
 
-    for value in calibration_values:
-        sum += value
-
-    return sum
-
-
-
-                
+    print(sum)
 
 if __name__ == '__main__':
     main()
