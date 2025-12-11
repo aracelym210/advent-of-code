69fc838e263f71b12b43ee9c8ef1ae5b4c4226d7
aracelym <2760319+aracelym210@users.noreply.github.com>
2025-12-10 21:49:57 -0600

    part2 solution for day 3

--- 2025/day3/solution.py
+++ 2025/day3/solution.py
@@ -2,19 +2,26 @@ def main():
     with open('input.txt', 'r') as f:
         battery_banks = [line.strip() for line in f.readlines()]
 
-    highest_jolts = []
+    highest_jolts_pt1 = []
+    highest_jolts_pt2 = []
     part1 = 0
+    part2 = 0
     for bank in battery_banks:
-        highest_jolts.append(find_highest_nums(bank))
+        highest_jolts_pt1.append(find_highest_nums(bank))
+        highest_jolts_pt2.append(find_highest_sequence(bank))
 
-    for jolts in highest_jolts:
+    for jolts in highest_jolts_pt1:
         part1 += jolts
 
+    for jolts in highest_jolts_pt2:
+        part2 += jolts
+
     print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")
 
 
 def find_highest_nums(bank: str) -> int:
-    battery = list(map(int, bank))  # convert string input into list of numbers 
+    battery = list(map(int, bank))  # convert string input into list of ints
 
     jolt_a = max(battery[:-1]) 
     jolt_a_index = battery.index(jolt_a)
@@ -24,6 +31,26 @@ def find_highest_nums(bank: str) -> int:
     jolt_b = max(right)
 
     return int(f"{jolt_a}{jolt_b}")
+
+
+def find_highest_sequence(bank: str) -> int:
+    battery = list(map(int, bank))
+
+    remaining_jolts = 11
+    jolts = ""
+
+    while remaining_jolts >= 0:
+        if remaining_jolts != 0:
+            jolt = max(battery[:-remaining_jolts])
+        else:
+            jolt = max(battery)
+
+        index = battery.index(jolt)
+        battery = battery[index + 1:]
+        remaining_jolts -= 1
+        jolts += str(jolt)
+
+    return int(jolts)
 
 
 if __name__ == "__main__":
